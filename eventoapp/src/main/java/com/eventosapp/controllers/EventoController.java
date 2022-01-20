c0fe8762e1ef1c0473dc5b67350b00ac2a0d1ba6
Isaac Portela <isaacportela2003@gmail.com>
2022-01-20 01:46:20 -0300

    Feita aula 10  e 11

--- eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
@@ -1,6 +1,8 @@
 package com.eventosapp.controllers;
 
+import com.eventosapp.models.Convidado;
 import com.eventosapp.models.Evento;
+import com.eventosapp.repository.ConvidadoRepository;
 import com.eventosapp.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class EventoController {
     @Autowired
     private EventoRepository er;
+
+    @Autowired
+    private ConvidadoRepository cr;
 
     @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
     public String form(){
@@ -31,11 +36,22 @@ public class EventoController {
         mv.addObject("eventos", eventos);
         return mv;
     }
-    @RequestMapping("/{codigo}")
+    @RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
     public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
         Evento evento = er.findByCodigo(codigo);
         ModelAndView mv = new ModelAndView("evento/detalhesEvento");
         mv.addObject("evento", evento);
+
+        Iterable<Convidado> convidados = cr.findByEvento(evento);
+        mv.addObject("convidados", convidados);
+
         return mv;
+    }
+    @RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
+    public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado){
+        Evento evento = er.findByCodigo(codigo);
+        convidado.setEvento(evento);
+        cr.save(convidado);
+        return "redirect:/{codigo}";
     }
 }
